a681d815c96effd5a9100107587cbfc451042cf7
yhcet <yhcet@globalcas.com>
2020-01-30 17:27:28 +0900

    Initial commit

---
+++ src/test/java/com/kosta/acme/AcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
